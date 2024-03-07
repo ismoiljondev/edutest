ac0b42230ee4a4ac10ba83ce0ed64bcadb074376
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2024-03-08 00:33:30 +0500

    last

--- src/Components/Layout/Navbar/Navbar.tsx
+++ src/Components/Layout/Navbar/Navbar.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import style from "./style.module.scss";
 import Link from "next/link";
 import Container from "@/Components/Container/Container";
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
 	return (
 		<div className={style.navbar}>
 			<Container>
 				<div className={style.nav}>
-					<p>logo</p>
-
-					<ul>
+					<div className={style.mainnav}>
+						<p>logo</p>
+						<div
+							style={{ width: "20px" }}
+							className={style.bars}
+							onClick={() => setOpen((prev) => !prev)}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 448 512"
+							>
+								<path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+							</svg>
+						</div>
+					</div>
+					<ul className={`${open ? style.menu : null}`}>
 						<li>
 							<Link href={"#feature"}>Feature</Link>
 						</li>
--- src/Components/Showcase/Showcase.tsx
+++ src/Components/Showcase/Showcase.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import style from "./style.module.scss";
+import { Button } from "..";
 const Showcase: React.FC = () => {
-	return <div className={style.showcase}></div>;
+	return (
+		<div className={style.showcase}>
+			<div className={style.showcase__text}>
+				<p>BEST ONLINE EDUCATION SERVICE IN THE WORLD</p>
+				<h2>ONE STEP AHEAD THIS SEASON</h2>
+				<div className={style.showcase__text__btns}>
+					<Button color="blue">LEARN MORE</Button>
+					<Button color="yellow">SEE COURSE</Button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Showcase;
